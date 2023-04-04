bb2e75c10a36726d8b042b7008af6e8bcc46caba
zacaryfettig <115122598+zacaryfettig@users.noreply.github.com>
2023-04-04 07:35:22 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "test"
+    storage_account_name = "test324234"
+    container_name       = "test"
+    key                  = "prod.terraform.tfstate"
+    use_oidc             = true
+    subscription_id      = "cb746d33-61f2-4005-a6e1-323245542be4"
+    tenant_id            = "09aedc25-108f-49fe-8f2a-8fcf474f365d"
+  }
+}
+
 resource "azurerm_resource_group" "resourceGroup" {
     name = var.resourceGroup
     location = var.location
