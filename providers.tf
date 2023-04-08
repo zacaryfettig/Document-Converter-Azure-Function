5f5c31351fc25fb3caf4280bb5cc866a47f1e77f
zacaryfettig <115122598+zacaryfettig@users.noreply.github.com>
2023-04-08 13:01:29 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -10,7 +10,5 @@ terraform {
 provider "azurerm" {
 
     features {}
-    subscription_id = "cb746d33-61f2-4005-a6e1-323245542be4"
-  tenant_id       = "09aedc25-108f-49fe-8f2a-8fcf474f365d"
     use_oidc = true
 }
