37cc2cc5c46cbd9d590283f0817fa9db45907bc4
zacaryfettig <115122598+zacaryfettig@users.noreply.github.com>
2023-04-08 16:06:03 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "resourceGroup" {
 }
 
 resource "azurerm_storage_account" "storageAccount" {
-  name                     = "docconverter-${random_id.random.result}"
+  name                     = "docconverter-${random_string.random.result}"
   resource_group_name      = var.resourceGroup
   location                 = var.location
   account_tier             = "Standard"
@@ -32,7 +32,7 @@ resource "azurerm_service_plan" "servicePlan" {
 }
 
 resource "azurerm_windows_function_app" "functionApp" {
-  name                = "docconverter${random_id.random.result}"
+  name                = "docconverter${random_string.random.result}"
   resource_group_name = var.resourceGroup
   location            = var.location
 
